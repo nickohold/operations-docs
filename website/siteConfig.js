5eb7499a397883e2e6d67cb0da8dbd0c94bf158d
Nick Holden <nick.holden@Nicks-MacBook-Pro.local>
2020-01-07 13:50:00 +0200

    baseurl fix

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -36,7 +36,7 @@ const siteConfig = {
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
    organizationName: 'nickohold',
    url:"https://nickohold.github.io/",
-   baseUrl:"/ops-docs/",
+   baseUrl:"/",
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
